7989608225cabca204745cd21a46b41acdfd9e97
victoriak94 <vkamila6@gmail.com>
2018-11-27 13:17:56 -0500

    Assigned the capaity of room 201 to a variable. Iterated through the hash used if statements to determine if the event matched the room id for room 201 and if the number of attendees fit the capacity of room 201. Completed assignment.

---
+++ reinforcement11.rb
@@ -0,0 +1,25 @@
+
+hash = { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+# Retrieve the capacity of room 201 and store it in a variable.
+p room_201_capacity = hash[:data][:rooms][0][:capacity]
+puts room_201_capacity
+
+# Find all the events taking place in room 201. Iterate through them and print "ok" if the number of planned attendees will fit in the room.
+hash[:data][:events].each do |event|
+  if event[:room_id] = 1
+    if event[:attendees] <= 50
+      puts "Ok"
+    end
+  end
+end
